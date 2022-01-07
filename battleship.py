5b9e02cfe9ba40cad82820e362e0c4802aa78f44
ReneArevalo13 <reneorlandoarevalo@gmail.com>
2022-01-06 19:41:47 -0800

    Added render function: renders board

--- battleship.py
+++ battleship.py
@@ -3,8 +3,18 @@
 
 def user_input():
     # function to initialize the game by asking where the player wants to shoot on the board
-    inp = input("Where do you want to shoot? [X,Y] \n") # comes in as a string
+    inp = input("Where do you want to shoot? [X,Y]\n") # comes in as a string
+    print(inp)
     x_string, y_string = inp.split(",") # split the input string at the "," and store
     x = int(x_string)
     y = int(y_string)
     return x,y
+
+
+def render_board(height, width):
+    # function to render the game board with for a given height and width
+    border = ("+" + "-" * width + "+")
+    print(border)
+    for i in range(height):
+        print("|" + " "*width + "|") 
+    print(border)
