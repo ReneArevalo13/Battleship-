f9f8ed9dda66fa2d46d3a62d90f5577138ad1dc7
ReneArevalo13 <reneorlandoarevalo@gmail.com>
2022-01-06 20:29:28 -0800

    implemented basic logic to run multiple shots by user

--- battleship.py
+++ battleship.py
@@ -1,10 +1,9 @@
 # Battleship game: one player vs computer
 
-
 def user_input():
     # function to initialize the game by asking where the player wants to shoot on the board
     inp = input("Where do you want to shoot? [X,Y]\n") # comes in as a string
-    print(inp)
+    #print(inp)
     x_string, y_string = inp.split(",") # split the input string at the "," and store
     x = int(x_string)
     y = int(y_string)
@@ -14,12 +13,13 @@ def user_input():
 def render_board(height, width, shots):
     # function to render the game board with for a given height and width
     border = ("+" + "-" * width + "+")
-    shots_set = set(shots)
     print(border)
-    for j in range(height): #iterate along the vertical, y-axis
+    shots_set = set(shots)
+    
+    for y in range(height): #iterate along the vertical, y-axis
         row = []
-        for i in range(width): #iterate along the horizontal, x-axis
-            if (i, j) in shots_set:
+        for x in range(width): #iterate along the horizontal, x-axis
+            if (x, y) in shots_set:
                 hit = "X" #symbol for ship being hit
             else:
                 hit = " " #empty space for a non hit
@@ -27,3 +27,13 @@ def render_board(height, width, shots):
 
         print("|" + "".join(row) + "|") 
     print(border)
+
+
+if __name__ == "__main__":
+    shots = []
+
+    while True:
+        shot = user_input()
+        shots.append(shot)
+        render_board(10, 10, shots)
+
