405c3f9adc4b0e10dd6b00185cf26c88a45342b5
ReneArevalo13 <reneorlandoarevalo@gmail.com>
2022-01-11 17:05:31 -0800

    Added rendering of board with battleships displayed

--- battleship.py
+++ battleship.py
@@ -1,5 +1,57 @@
 # Battleship game: one player vs computer
 
+class Battleship(object):
+
+    @staticmethod
+    def build(head, length, direction):
+        #method to build an instance of a battleship
+        #head:coordinate on game grid of where the head of the ship should be   
+        #length: how long (how many spaces should the ship span)
+        #direction: in what direction should the ship go in N,E,S,W 
+        # ex: b1 = Battleship.build((1,1), 5, "N"), head at (1,1), length of 5 pointing North 
+        #        
+        body = [] #empty list to build the body of the battleship
+
+        #logic to add length to the body in the given direction
+        for i in range(length):
+            if direction == "N":
+                element = (head[0], head[1] - i)
+            elif direction == "S":
+                element = (head[0], head[1] + i)
+            elif direction == "E":
+                element = (head[0] - i, head[1])
+            elif direction == "W":
+                element = (head[0] + i, head[1])
+            
+            body.append(element)
+        
+        return Battleship(body)
+
+        
+
+    def __init__(self, body):
+        self.body = body
+
+def render_battleship(width, height, battleships):
+    border = ("+" + "-" * width + "+")
+    print(border)
+    board = []
+
+    #create empty board
+    for x in range(width):
+        board.append(" " for y in range(height))
+
+    for b in battleships:
+        for x, y in b.body:
+            board[x][y] = "O"
+
+
+
+    print(board)
+    print(border)
+    
+     
+
 def user_input():
     # function to initialize the game by asking where the player wants to shoot on the board
     inp = input("Where do you want to shoot? [X,Y]\n") # comes in as a string
@@ -29,11 +81,14 @@ def render_board(height, width, shots):
     print(border)
 
 
-if __name__ == "__main__":
-    shots = []
 
-    while True:
-        shot = user_input()
-        shots.append(shot)
-        render_board(10, 10, shots)
 
+#if __name__ == "__main__":
+
+ 
+   # shots = []
+
+   # while True:
+  #     shot = user_input()
+   #     shots.append(shot)
+   #     render_board(10, 10, shots)
