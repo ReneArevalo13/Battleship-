a519cab27d3a64c311f31a22372199b61d7f0c4d
ReneArevalo13 <reneorlandoarevalo@gmail.com>
2022-01-12 20:42:13 -0800

    Added display of hits and misses, as well as endgame conditions

--- battleship.py
+++ battleship.py
@@ -19,18 +19,66 @@ class Battleship(object):
             elif direction == "S":
                 element = (head[0], head[1] + i)
             elif direction == "E":
-                element = (head[0] - i, head[1])
-            elif direction == "W":
                 element = (head[0] + i, head[1])
+            elif direction == "W":
+                element = (head[0] - i, head[1])
             
             body.append(element)
         
         return Battleship(body)
 
-        
-
     def __init__(self, body):
         self.body = body
+        self.hits = [False] * len(body) #create array that holds hits, when hit, False -> True
+
+    def body_index(self, location):
+        try:
+            return self.body.index(location)
+        except ValueError:
+            return None
+    def is_destroyed(self):
+        return all(self.hits)
+
+class GameState(object):
+    #class to control the gamestate 
+    #battleships generated at beginning of game and passed in (not changing)
+    #input board dimesions height, width
+    #include empty shots list to hold shots 
+
+    def __init__(self, battleships, height, width):
+        self.battleships = battleships
+        self.width = width
+        self.height = height
+        self.shots = []
+    
+    def take_shot(self, shot_location):
+        #function to update whether battleship was hit
+        is_hit = False
+        for b in self.battleships:
+            idx = b.body_index(shot_location)
+            if idx is not None:
+                is_hit = True
+                b.hits[idx] = True
+                break
+        self.shots.append(Shot(shot_location, is_hit))
+    
+    def endgame(self):
+        #for each battleship is the ship destroyed
+        #if yes return True, else False
+        return all([b.is_destroyed() for b in self.battleships])
+
+
+
+
+
+
+
+class Shot(object):
+    #class to hold hit/miss
+    def __init__(self, location, is_hit):
+        self.location = location
+        self.is_hit = is_hit
+
 
 def render_battleship(width, height, battleships):
     border = ("+" + "-" * width + "+")
@@ -39,29 +87,26 @@ def render_battleship(width, height, battleships):
 
     #create empty board
     for x in range(width):
-        board.append(" " for y in range(height))
+        row = []
+        for y in range(height):
+            row.append(None)
+        board.append(row)
 
+    #add battleships to the board with character
     for b in battleships:
         for x, y in b.body:
-            board[x][y] = "O"
+            board[x][y] = "*"
 
+    # go through the rows and append the newly added ships to the board
+    for y in range(height):
+        row = []
+        for x in range(width):
+            row.append(board[x][y] or " ")
+        print("|" + "".join(row) + "|")
 
-
-    print(board)
     print(border)
     
      
-
-def user_input():
-    # function to initialize the game by asking where the player wants to shoot on the board
-    inp = input("Where do you want to shoot? [X,Y]\n") # comes in as a string
-    #print(inp)
-    x_string, y_string = inp.split(",") # split the input string at the "," and store
-    x = int(x_string)
-    y = int(y_string)
-    return (x,y) #output as a tuple
-
-
 def render_board(height, width, shots):
     # function to render the game board with for a given height and width
     border = ("+" + "-" * width + "+")
@@ -81,10 +126,105 @@ def render_board(height, width, shots):
     print(border)
 
 
+def render(game, show_battleships = False):
+    # function to render the game board with for a given height and width
+    border = ("+" + "-" * game.width + "+")
+    print(border)
+    board = []
+
+    #create empty board
+    for x in range(game.width):
+        row = []
+        for y in range(game.height):
+            row.append(None)
+        board.append(row)
+    
+    #add battleships to the board with character
+    if show_battleships:
+        for b in game.battleships:
+            for x, y in b.body:
+                board[x][y] = "~"
+    
+    #add shots taken to the game board
+    for s in game.shots:
+        x, y = s.location
+        if s.is_hit:
+            ch = "H" #represents a hit
+            #print("HIT")
+        else:
+            ch = "M" #represents a miss
+            #print("MISS")
+        board[x][y] = ch
 
 
-#if __name__ == "__main__":
+    # go through the rows and append the newly added ships to the board
+    for y in range(game.height):
+        row = []
+        for x in range(game.width):
+            row.append(board[x][y] or " ")
+        print("|" + "".join(row) + "|")
 
+    print(border)
+    
+
+
+
+def user_input():
+    # function to initialize the game by asking where the player wants to shoot on the board
+    inp = input("Where do you want to shoot? [X,Y]\n") # comes in as a string
+    #print(inp)
+    x_string, y_string = inp.split(",") # split the input string at the "," and store
+    x = int(x_string)
+    y = int(y_string)
+    return x, y #output as a tuple
+
+
+if __name__ == "__main__":
+    battleships = [Battleship.build((1,1), 2, "N")]
+                    # ,Battleship.build((5,8), 5, "N"),
+                    # Battleship.build((2,3), 4, "E")]
+
+    game = GameState(battleships, 10, 10)
+    # shots = [(1,1), (0,0), (5,7)]
+
+    # for sh in shots:
+    #     game.take_shot(sh)
+
+    #main game loop
+    while True:
+        x, y = user_input()
+        game.take_shot((x,y))
+        render(game)
+
+        #ENDGAME
+        if game.endgame():
+            print("YOU WIN!!")
+            break
+
+
+
+
+        
+    
+    
+
+
+
+
+
+
+
+
+# for sh in game.shots:
+#     print(sh.location)
+#     print(sh.is_hit)
+#     print("_____")
+
+# for b in game.battleships:
+#     print(b.body)
+#     print(b.hits)
+#     print("_____")
+            
  
    # shots = []
 
